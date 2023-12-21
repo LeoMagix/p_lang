ac12c6c07286cb105ffc341bd76ad303c570bab0
LeoMagix <Dahleo08@gmail.com>
2023-12-21 07:14:20 +0100

    made changes to list_functions.py

--- p_lang/functions/module/list_functions.py
+++ p_lang/functions/module/list_functions.py
@@ -1,24 +1,23 @@
 """This is a program to pass a list as a parameter to a function."""
 
-'''
+
 #Start with some designs that need to be printed
-unprinted_designs = ['phone case', 'pendant', 'robot']
-completed_models = []
+#unprinted_designs = ['phone case', 'pendant', 'robot']
+#completed_models = []
 
 #Simulate printing each design until none is left
 #move each design to completed_models after printing
 while unprinted_designs:
-    current_design = unprinted_designs.pop()
-    print(f"Printing Model: {current_design}")
-    completed_models.append(current_design)
+    #current_design = unprinted_designs.pop()
+    #print(f"Printing Model: {current_design}")
+    #completed_models.append(current_design)
 
 #Display all completed models
-print("The following models have been printed:")
-for completed_model in completed_models:
-    print(completed_model)
-'''
+#print("The following models have been printed:")
+#for completed_model in completed_models:
+    #print(completed_model)
 
-#The same program optimsed more efficiently using a function
+#The Same Program made More efficient With a Function
 def print_models(unprinted_designs, completed_models):
     """
     Simulate printing each design, until none are left.
