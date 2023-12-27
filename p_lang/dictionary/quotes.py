3a41f2b4e203000a86fefb8ec9c7d5e73adfb569
LeoMagix <Dahleo08@gmail.com>
2023-12-27 14:21:21 +0100

    create file, quotes.py

---
+++ p_lang/dictionary/quotes.py
@@ -0,0 +1,25 @@
+"""Writing a program that stores dictionary in a list."""
+
+viking = f'"Odin gave his left eye to acquire knowledge, I will give far more to acquire it."'
+ragner = {'ragner lothbrok': viking}
+
+mindset = f'"Fear just like limits, is nothing but an illusion."'
+mj = {'michael jordan': mindset}
+
+work = f'"Competition is the easy part, the real work happens behind the scenes."'
+bolt = {'usain bolt' : work}
+
+great_quotes = [ragner, mj, bolt]
+#print(great_quotes)
+
+for greats in great_quotes:
+    for great, quotes in sorted(greats.items()):
+        print(quotes)
+        if great == 'michael jordan':
+            print(f'\t\t\t\t\t-{great.title()}\n')
+        
+        elif great == 'usain bolt':
+            print(f'\t\t\t\t\t\t\t\t-{great.title()}\n')
+        
+        else:
+            print(f'\t\t\t\t\t\t\t\t\t-{great.title()}\n')
