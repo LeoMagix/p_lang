0e2ee0f0bdedb4e76247da3e930271b50883472b
LeoMagix <dahleo08@gmail.com>
2026-05-18 17:57:18 +0100

    Refactored code in collatz.py

--- p_lang/functions/collatz.py
+++ p_lang/functions/collatz.py
@@ -12,23 +12,40 @@ def collatz(number):
     else:
         return 3 * number + 1
 
-#Write a code that keeps calling collatz until its return value is equal to one(1).
-try:
-    #We ask the user for the integer number
-    num = int(input("Please Enter A Number:\n"))
+# Call collatz until its return value is equal to one(1).
+# Refactoring the code that handles the user's input and collatz() function call.
+# We are putting the code that prompts user for a number into the function call_collatz().
 
-except ValueError:
-    print("Enter an Integer")
+def call_collatz():
+    """
+    This function requests User to enter an integer; value entered must be a number.
+    Employ a Try-Except block to catch all errorneous or malicious inputs.
+    Return the computed collatz number to the User.
+    """
+    # Ask User to enter a number
+    try:
+        # Checks if User entered a number 
+        # If they have, then convert it to an integer
+        int_num = int(input("Please Enter A Number:\n"))
 
-else:
-    while True:
-        collatz_num = collatz(num)
+    # Print a warning if the value is anything but a number
+    except ValueError:
+        print("Wrong value.\nEnter an Integer")
+
+    # Main code that utilizes the collatz() function
+    else:
+        while True:
+            collatz_num = collatz(int_num)
     
-        if collatz_num == 1:
-            print(collatz_num)
-            break
+            if collatz_num == 1:
+                return collatz_num
+                #break
     
-        else:
-             print(collatz_num)
-             num = collatz_num
-         
+            else:
+                print(f"COLLATZ NUMBER=={collatz_num}")
+                int_num = collatz_num   #Sets int_num to new value
+
+ret_num = call_collatz()
+if ret_num != None:
+    print(f"COLLATZ NUMBER--{ret_num}")
+#print(ret_num)
