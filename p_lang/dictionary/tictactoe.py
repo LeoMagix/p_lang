75ad5b64ddee85518d4c877b6a02153dbf7e9b1f
LeoMagix <dahleo08@gmail.com>
2026-06-08 18:02:29 +0100

    Moved files to dictionary folder

---
+++ p_lang/dictionary/tictactoe.py
@@ -0,0 +1,34 @@
+"""
+Simple Python that creates the preliminary for a Tic-Tac-Toe game.
+Some features I would like to add, subsequently:
+    >> Announce when we have a winner or a tie.
+    >> Prevent a player from choosing a positon already chozen by another player.
+    >> A mini annoucement at the start with info on how to place postions.
+    >> Check/handle invalid input.
+"""
+
+theboard = {'top-L':' ', 'top-M':' ', 'top-R':' ',
+            'mid-L':' ', 'mid-M':' ', 'mid-R':' ',
+            'bot-L':' ', 'bot-M':' ', 'bot-R':' '}
+
+#Function to handle display of tic-tac-toe grid to the screen
+def printBoard(board):
+    """Describes how the inputs are going to be represented."""
+    print(f"{board['top-L']}|{board['top-M']}|{board['top-R']}")
+    print("-+-+-")
+    print(f"{board['mid-L']}|{board['mid-M']}|{board['mid-R']}")
+    print(f"-+-+-")
+    print(f"{board['bot-L']}|{board['bot-M']}|{board['bot-R']}")
+
+turn = 'X'
+for i in range(9):
+    printBoard(theboard)            #This helps display the current situation of the board
+    print(f"It's your turn to move Player:{turn}")
+    move = input()
+    theboard[move] = turn.upper()
+    if turn.upper() == 'X':             #sets variable turn to 'O' after player X chooses
+        turn = 'O'
+    else:
+        turn = 'X'                      #Assigns X to the variable turn after player O chooses
+
+printBoard(theboard)
